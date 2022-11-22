33d4a031c8d81dc397da8ed8e55619183e19a2cd
unknown <amansariya@gmail.com>
2022-11-21 18:14:11 -0800

    Added sound, spike timeout and scaled

--- Assets/Script/DetectedFeedback.cs
+++ Assets/Script/DetectedFeedback.cs
@@ -9,13 +9,30 @@ public class DetectedFeedback : MonoBehaviour
     // Start is called before the first frame update
     public float timeAmount;
 
-    [SerializeField] private ObjectDelayEntry[] objectDelayEntries;
+    //[SerializeField]
+    //private ObjectDelayEntry[] objectDelayEntries;
+
+    [SerializeField]
+    public GameObject[] spikeElements;
+
+    public float spikeTime;
+
+    IEnumerator ToggleSpikes(GameObject[] gobj,float delay)
+    {
+        for (int i = 0; i < gobj.Length; i++)
+        {
+            gobj[i].SetActive(true);
+        }
+        yield return new WaitForSeconds(delay);
+        for (int i = 0; i < gobj.Length; i++)
+        {
+            gobj[i].SetActive(false);
+        }
+    }
 
     IEnumerator ToggleFeedback(float delay)
     {
-        yield return new WaitForSeconds(delay);
         timeBasedFeedback.SetActive(true);
-
         yield return new WaitForSeconds(delay);
         timeBasedFeedback.SetActive(false);
     }
@@ -23,6 +40,11 @@ public class DetectedFeedback : MonoBehaviour
     public void StartToggleFeedback()
     {
         StartCoroutine(ToggleFeedback(timeAmount));
+    }
+
+    public void SpikeDisplay()
+    {
+        StartCoroutine(ToggleSpikes(spikeElements, spikeTime));
     }
 
     [Serializable]
